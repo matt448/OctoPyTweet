d773513b6d21ff8900dfa42a3a1c043d4d2c4531
Matthew McMillan <matthew.mcmillan@gmail.com>
2020-08-28 10:10:20 -0500

    whitespace fix, ignore .vscode directory

--- OctoPyTweet.py
+++ OctoPyTweet.py
@@ -9,7 +9,7 @@
 ###############
 # Description
 #
-# This is a simple script to tweet stats and pictures for print jobs 
+# This is a simple script to tweet stats and pictures for print jobs
 # running on an OctoPrint print server. It is called from cron once a minute.
 # This would be better if it was an OctoPrint plug-in but what can you do.
 # It works and I don't have time to learn how to make a plug-in right now.
